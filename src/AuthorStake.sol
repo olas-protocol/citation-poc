a81b0850a2d0b0bea10f11df84121e91bf3a524c
Kris O'Shea <minimallifter@protonmail.com>
2024-01-29 15:06:11 +0000

    Royalty Resolver and Staking contracts

---
+++ src/AuthorStake.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+/// @title AuthorStakingContract
+/// @notice A simple contract for staking Ether.
+contract AuthorStakingContract is ReentrancyGuard {
+    // Mapping to track staked Ether balances of each address.
+    mapping(address => uint256) public stakes;
+    // Event to log staking activity.
+
+    event EtherStaked(address indexed from, uint256 amount);
+    // Event to log withdrawal activity.
+    event EtherWithdrawn(address indexed to, uint256 amount);
+
+    // Modifier to check if the caller has enough staked balance.
+    modifier hasSufficientStake(uint256 amount) {
+        require(
+            stakes[msg.sender] >= amount,
+            "Insufficient staked amount to withdraw specified amount"
+        );
+        _;
+    }
+
+    function stakeEther() public payable nonReentrant {
+        require(msg.value > 0, "Must send Ether to stake");
+        stakes[msg.sender] += msg.value;
+        emit EtherStaked(msg.sender, msg.value);
+    }
+
+    // Allows users to withdraw their staked Ether.
+    function withdrawStake(
+        uint256 amount
+    ) external nonReentrant hasSufficientStake(amount) {
+        require(amount > 0, "Withdrawal amount must be greater than 0");
+        stakes[msg.sender] -= amount;
+        Address.sendValue(payable(msg.sender), amount); // Safer Ether transfer
+        emit EtherWithdrawn(msg.sender, amount);
+    }
+
+    // Function to check the staked balance of a caller.
+    function getStakedBalance(address staker) external view returns (uint256) {
+        return stakes[staker];
+    }
+
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {
+        stakeEther();
+    }
+}
--- src/RoyaltyResolver.sol
+++ src/RoyaltyResolver.sol
@@ -1,49 +1,118 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.19;
 
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
-
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {SchemaResolver} from "eas-contracts/resolver/SchemaResolver.sol";
-
 import {IEAS, Attestation} from "eas-contracts/IEAS.sol";
+import {IAuthorStake} from "./interfaces/IAuthorStake.sol";
 
-/// @title PayingResolver
-/// @notice A sample schema resolver that pays attesters (and expects the payment to be returned during revocations).
-contract RoyaltyResolver is SchemaResolver {
+/// @notice A struct representing the additional custom Olas attestation fields.
+struct CustomAttestationSchema {
+    bytes32[] citationUID; // An array of citation UIDs
+    bytes32 authorName; // The author's name
+    string articleTitle; // The title of the article
+    bytes32 articleHash; // A hash of the article content
+    string urlOfContent; // The URL where the content can be accessed
+}
+
+/// @title RoyaltyResolver
+/// @notice Distributes royalties among citation attesters and stakes the remaining ether on behalf of the attester.
+contract RoyaltyResolver is SchemaResolver, ReentrancyGuard {
     using Address for address payable;
 
-    error InvalidValue();
+    error InsufficientEthValueSent();
+    error InvalidCitationUID();
+    error InsufficientIndividualRoyaltyPayment();
+    error DirectPaymentsNotAllowed();
 
-    uint256 private immutable _incentive;
+    event RoyaltyDistributed(address indexed receiver, uint256 amount);
+    event TransferredStake(address indexed stakingContract, uint256 amount);
+    event ValueReceived(address indexed attester, uint256 value);
 
-    constructor(IEAS eas, uint256 incentive) SchemaResolver(eas) {
-        _incentive = incentive;
+    address private immutable _stakingContract;
+    uint256 private constant ROYALTY_PERCENTAGE = 10; // Represents the royalty percentage.
+
+    constructor(IEAS eas, address stakingContract) SchemaResolver(eas) {
+        require(
+            stakingContract != address(0),
+            "Invalid staking contract address"
+        );
+        _stakingContract = stakingContract;
+        _eas = eas;
     }
 
     function isPayable() public pure override returns (bool) {
         return true;
     }
 
-    function onAttest(Attestation calldata attestation, uint256 value) internal override returns (bool) {
-        if (value > 0) {
-            return false;
+    // Decodes the custom Olas schema data from the standard attestation schema's data field
+    function decodeCustomData(
+        bytes memory data
+    ) private pure returns (CustomAttestationSchema memory) {
+        return abi.decode(data, (CustomAttestationSchema));
+    }
+
+    /// @param value = article stake amount
+    function onAttest(
+        Attestation calldata attestation,
+        uint256 value
+    ) internal override nonReentrant returns (bool) {
+        if (value == 0) revert InsufficientEthValueSent();
+        emit ValueReceived(msg.sender, value);
+
+        // Decode the attestation's data field into a struct
+        CustomAttestationSchema memory customData = decodeCustomData(
+            attestation.data
+        );
+
+        uint256 receiversUIDsListLength = customData.citationUID.length;
+        if (receiversUIDsListLength == 0) {
+            return true;
         }
 
-        payable(attestation.attester).transfer(_incentive);
+        uint256 totalRoyalty = (value * ROYALTY_PERCENTAGE) / 100;
+        uint256 individualRoyalty = totalRoyalty / receiversUIDsListLength;
+        if (individualRoyalty == 0) {
+            revert InsufficientIndividualRoyaltyPayment();
+        }
+
+        uint256 stakingAmount = value - totalRoyalty; // Calculate the remaining staking amount after deducting royalty fee and transfer to the staking contract.
+
+        for (uint256 i = 0; i < receiversUIDsListLength; ++i) {
+            // Access each citationUID from the decoded data
+            bytes32 citationUID = customData.citationUID[i];
+            // Fetch the attestation for each citationUID
+            Attestation memory receiverAttestation = _eas.getAttestation(
+                citationUID
+            );
+            if (address(receiverAttestation.attester) == address(0)) {
+                revert InvalidCitationUID();
+            }
+
+            address payable royaltyReceiverAddress = payable(
+                receiverAttestation.attester
+            );
+            royaltyReceiverAddress.sendValue(individualRoyalty); // Using OpenZeppelin's sendValue() for safe Eth transfer.
+            emit RoyaltyDistributed(royaltyReceiverAddress, individualRoyalty);
+        }
+
+        IAuthorStake(_stakingContract).stakeEther{value: stakingAmount}();
+        emit TransferredStake(_stakingContract, stakingAmount);
 
         return true;
     }
 
-    function onRevoke(Attestation calldata attestation, uint256 value) internal override returns (bool) {
-        if (value < _incentive) {
-            return false;
-        }
+    function onRevoke(
+        Attestation calldata attestation,
+        uint256 value
+    ) internal override returns (bool) {}
 
-        if (value > _incentive) {
-            payable(address(attestation.attester)).sendValue(value - _incentive);
-        }
-
-        return true;
+    receive() external payable override {
+        revert DirectPaymentsNotAllowed();
     }
 }
+
+// 1: add Solady safeTransfer
+// 2: check for potential integer overflow / underflow issues when calculating the individualRoyalty
+// 4: add import specific contract versions
